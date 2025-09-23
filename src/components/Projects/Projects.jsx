e88031bc2353c7b623ab4f19bd086cd7642bfa73
Nishant Raj <nishantraj7859@gmail.com>
2025-09-24 00:12:47 +0530

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import Project2 from '../../assets/project2.png'
 import Project3 from '../../assets/project3.png'
 import Project4 from '../../assets/project4.png'
 import Project5 from '../../assets/project5.png'
-import Project6 from '../../assets/project-6.png'
+import Project6 from '../../assets/Project-6.png'
 import { FaArrowRight } from 'react-icons/fa'
 
 const projectsData = [
